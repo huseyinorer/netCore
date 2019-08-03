cdc58a892a3f6fb87d68616d26caf35ad358c98f
Hüseyin Örer <huseyinorer@yandex.com>
2019-08-03 15:36:27 +0300

    added main project

---
+++ MainProject/Identity/AppIdentityDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MainProject.Identity
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppIdentityUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+        {
+           
+        }
+    }
+}
