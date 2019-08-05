5228c6c0d61ad2a5fb7ad9fef75ae9acf0aa1917
Hüseyin Örer <huseyinorer@yandex.com>
2019-08-05 22:55:56 +0300

    Derslere devam

---
+++ MainProject/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MainProject.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MainProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppIdentityUser> _userManager;
+
+        public AdminController(UserManager<AppIdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View(_userManager.Users.ToList());
+        }
+    }
+}
