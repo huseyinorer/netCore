378ec95e08fd6ede288e5355604e2de8f1cb1e05
Hüseyin Örer <huseyinorer@yandex.com>
2019-08-29 11:20:41 +0300

    ttt2

--- MainProject/Startup.cs
+++ MainProject/Startup.cs
@@ -53,7 +53,7 @@ namespace MainProject
                 
 
             });
-            //ttt
+            //tttt
             services.AddSession();
             services.AddDistributedMemoryCache();//session nerede tutulacak : uygulmaa sunucusu hafızası
             services.ConfigureApplicationCookie(options => {//ders 61
