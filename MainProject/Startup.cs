63c935ed7c2765b824cffc00deb84be8803faada
Hüseyin Örer <huseyinorer@yandex.com>
2021-02-25 23:18:03 +0300

    authe

--- MainProject/Startup.cs
+++ MainProject/Startup.cs
@@ -52,19 +52,19 @@ namespace MainProject
             services.AddEntityFrameworkNpgsql().AddDbContext<ProjectDbContext>(options => options.UseNpgsql(_configuration["DbConnection"]));//ders:62
 
             //facebook ile authentication.
-            services.AddAuthentication()
-                .AddFacebook(opts =>
-                {
+            //services.AddAuthentication()
+            //    .AddFacebook(opts =>
+            //    {
 
-                    opts.AppId = _configuration["Authentication:Facebook:AppId"];
-                    opts.AppSecret = _configuration["Authentication:Facebook:AppSecret"];
+            //        opts.AppId = _configuration["Authentication:Facebook:AppId"];
+            //        opts.AppSecret = _configuration["Authentication:Facebook:AppSecret"];
 
-                }).AddGoogle(opts =>
-                {
-                    opts.ClientId = _configuration["Authentication:Google:ClientID"];
-                    opts.ClientSecret = _configuration["Authentication:Google:ClientSecret"];
+            //    }).AddGoogle(opts =>
+            //    {
+            //        opts.ClientId = _configuration["Authentication:Google:ClientID"];
+            //        opts.ClientSecret = _configuration["Authentication:Google:ClientSecret"];
 
-                });
+            //    });
             services.AddIdentity<AppIdentityUser, AppIdentityRole>()
                 .AddEntityFrameworkStores<AppIdentityDbContext>()
                 .AddDefaultTokenProviders()
